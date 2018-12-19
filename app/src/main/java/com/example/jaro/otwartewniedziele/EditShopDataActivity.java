d0c31e4f420cba6706ea6ff934e96cf42ef4e479
jaroslawdrzaszcz <s16136@pjwstk.edu.pl>
2018-12-19 10:55:18 +0100

    aaaa

---
+++ app/src/main/java/com/example/jaro/otwartewniedziele/EditShopDataActivity.java
@@ -0,0 +1,13 @@
+package com.example.jaro.otwartewniedziele;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EditShopDataActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate ( savedInstanceState );
+        setContentView ( R.layout.activity_edit_shop_data );
+    }
+}
---
+++ app/src/main/java/com/example/jaro/otwartewniedziele/InputEditActivity.java
@@ -0,0 +1,13 @@
+package com.example.jaro.otwartewniedziele;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InputEditActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate ( savedInstanceState );
+        setContentView ( R.layout.activity_input_edit );
+    }
+}
