2a5c4cfdf38124ae5136a510aa4d4860976f3fa5
jaroslawdrzaszcz <s16136@pjwstk.edu.pl>
2018-12-19 21:17:38 +0100

    some more screens

--- app/src/main/java/com/example/jaro/otwartewniedziele/ByAdress.java
+++ app/src/main/java/com/example/jaro/otwartewniedziele/ByAdress.java
@@ -1,7 +1,10 @@
 package com.example.jaro.otwartewniedziele;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ByAdress extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class ByAdress extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate ( savedInstanceState );
         setContentView ( R.layout.activity_by_adress );
+
+        final Button btn1 = findViewById(R.id.back);
+        btn1.setOnClickListener(new View.OnClickListener (){
+            public void onClick(View v) {
+                Intent i = new Intent ( getApplicationContext (), MapActivity.class );
+                startActivity (i);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/jaro/otwartewniedziele/MainActivity.java
+++ app/src/main/java/com/example/jaro/otwartewniedziele/MainActivity.java
@@ -22,10 +22,26 @@ public class MainActivity extends AppCompatActivity {
         });
 
         final Button btn2 = findViewById(R.id.Zamknij);
-        btn1.setOnClickListener(new View.OnClickListener (){
+        btn2.setOnClickListener(new View.OnClickListener (){
             public void onClick(View v) {
                 finish ();
                 System.exit ( 0 );
+            }
+        });
+
+        final Button btn3 = findViewById(R.id.PortalSprzedawcy);
+        btn3.setOnClickListener(new View.OnClickListener (){
+            public void onClick(View v) {
+                Intent i = new Intent ( getApplicationContext (), LoginActivity.class );
+                startActivity (i);
+            }
+        });
+
+        final Button btn4 = findViewById(R.id.SklepyWOkolicy);
+        btn4.setOnClickListener(new View.OnClickListener (){
+            public void onClick(View v) {
+                Intent i = new Intent ( getApplicationContext (), MapActivity.class );
+                startActivity (i);
             }
         });
     }
