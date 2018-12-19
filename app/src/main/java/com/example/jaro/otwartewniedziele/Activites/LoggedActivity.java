5bdb363a1f21bc57d8c1bc16d5dfff4563037394
jaroslawdrzaszcz <s16136@pjwstk.edu.pl>
2018-12-19 22:55:56 +0100

    more

--- app/src/main/java/com/example/jaro/otwartewniedziele/Activites/LoggedActivity.java
+++ app/src/main/java/com/example/jaro/otwartewniedziele/Activites/LoggedActivity.java
@@ -1,7 +1,10 @@
 package com.example.jaro.otwartewniedziele.Activites;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.jaro.otwartewniedziele.R;
 
@@ -11,5 +14,37 @@ public class LoggedActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate ( savedInstanceState );
         setContentView ( R.layout.activity_logged );
+
+        final Button btn1 = findViewById(R.id.back6);
+        btn1.setOnClickListener(new View.OnClickListener (){
+            public void onClick(View v) {
+                Intent i = new Intent (getApplicationContext (), InputEditShopownerDataActivity.class);
+                startActivity ( i );
+            }
+        });
+
+        final Button btn2 = findViewById(R.id.mainPage6);
+        btn2.setOnClickListener(new View.OnClickListener (){
+            public void onClick(View v) {
+                Intent i = new Intent ( getApplicationContext (), MainActivity.class );
+                startActivity (i);
+            }
+        });
+
+        final Button btn3 = findViewById(R.id.AddShop);
+        btn3.setOnClickListener(new View.OnClickListener (){
+            public void onClick(View v) {
+                Intent i = new Intent ( getApplicationContext (), InputShopDataActivity.class );
+                startActivity (i);
+            }
+        });
+
+        final Button btn4 = findViewById(R.id.YourShops);
+        btn4.setOnClickListener(new View.OnClickListener (){
+            public void onClick(View v) {
+                Intent i = new Intent ( getApplicationContext (), LoggedActivity.class );
+                startActivity (i);
+            }
+        });
     }
 }
